32400624e5b600fe2fc315e91b91d727acb86eae
Aamishsn <108685985+Aamishsn@users.noreply.github.com>
2023-06-28 23:10:16 +0500

    Add files via upload

---
+++ myapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class Booking(models.Model):
+    first_name = models.CharField(max_length=200)
+    reservation_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self):
+        return self.first_name
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.title
